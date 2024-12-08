ca2da9b1ebdf64053fa9890524b93582096a97b0
riky.fidanza <riky.fidanza@gmail.com>
2024-12-08 15:36:22 +0100

    .

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
-import java.util.Set;
-
 public class Main {
 
     public static void main(String[] args) {
-        // Define a sample formula
+        
         String formula = "f(a,b) = a & f(f(a,b),b) ! a";
         EqualitySolver solver = new EqualitySolver();
         solver.solve(formula);
